b5a31876a60c945154180be25056a9c192b22f75
Tony <tony.hok-xualet@edu.itescia.fr>
2020-03-18 14:35:34 +0100

    First commit

---
+++ app/src/main/java/com/tony/roadie/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tony.roadie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
