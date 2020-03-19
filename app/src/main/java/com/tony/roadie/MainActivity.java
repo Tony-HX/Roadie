59e18e4754e25dfa9b365da81afc9cb1b782c2e7
Tony <tony.hok-xualet@edu.itescia.fr>
2020-03-20 00:15:57 +0100

    Story 5: button changes when answer is correct and sends to same activity. Didn't manage to putExtra the new question

--- app/src/main/java/com/tony/roadie/MainActivity.java
+++ app/src/main/java/com/tony/roadie/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -26,7 +28,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //adds RadioButtons to the RadioGroup on Create then shuffles the order randomly
+        final Question q = new Question("Comment s'appelle le batteur des Beatles?", R.drawable.the_beatles, "Ringo Starr", "John Frusciante", "Les Paul", "George Michael");
+        final Question q2 = new Question("Comment s'appelle le batteur de Nirvana?", R.drawable.nirvana, "Dave Grohl", "Krist Novoselic", "Steve Shelley", "Jack Irons");
+        final TextView questionTextView = findViewById(R.id.questionTextView);
+        final String question = q.getQuestion();
+        questionTextView.setText(question);
+
+        final ImageView questionImageView = findViewById(R.id.questionImageView);
+        final int image = q.getImage();
+        questionImageView.setImageResource(image);
+
+        //links the ImageView to ImageActivity
+        questionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
+
+                //sends current image (launcher background) to ImageActivity
+                intent.putExtra("imageId", image);
+
+                startActivity(intent);
+            }
+        });
+
+        //adds RadioButtons to the RadioGroup on Create then shuffles their order randomly
         radioGroup = findViewById(R.id.answersRadioGroup);
         List<RadioButton> answers = new ArrayList<>();
         RadioButton answerRadioButton1 = new RadioButton(this);
@@ -37,45 +62,44 @@ public class MainActivity extends AppCompatActivity {
         answers.add(answerRadioButton2);
         answers.add(answerRadioButton3);
         answers.add(answerRadioButton4);
-        answerRadioButton1.setText("A");
-        answerRadioButton2.setText("B");
-        answerRadioButton3.setText("C");
-        answerRadioButton4.setText("D");
+        answerRadioButton1.setText(q.getItem1());
+        answerRadioButton2.setText(q.getItem2());
+        answerRadioButton3.setText(q.getItem3());
+        answerRadioButton4.setText(q.getItem4());
         Collections.shuffle(answers);
         for(RadioButton answerRadioButton:
                 answers){
             radioGroup.addView(answerRadioButton, new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         }
 
-        Button submitButton = findViewById(R.id.submitButton);
+        final Button submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int radioId = radioGroup.getCheckedRadioButtonId();
                 radioButton = findViewById(radioId);
+                String rightAnswer = q.getItem1();
 
-                //shows a Toast saying whether the answer is correct or not
-                if(radioButton.getText().equals("A")){
-                    Toast.makeText(MainActivity.this, "A: Bonne réponse!", Toast.LENGTH_SHORT).show();
+                //shows a Toast saying whether the answer is correct or not, transforms the submitButton
+                if(radioButton.getText().equals(rightAnswer)){
+                    Toast.makeText(MainActivity.this, "A: Bonne réponse!", Toast.LENGTH_LONG).show();
+                    submitButton.setText("Question suivante");
+                    submitButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+                            startActivity(intent);
+
+                        }
+                    });
                 }else{
-                    Toast.makeText(MainActivity.this, "Mauvaise réponse! C'est la A.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Mauvaise réponse! C'est " + rightAnswer + ".", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        //links the ImageView to ImageActivity
-        ImageView questionImageView = findViewById(R.id.questionImageView);
-        questionImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
 
-                //sends current image (launcher background) to ImageActivity
-                intent.putExtra("imageId", R.drawable.ic_launcher_background);
-
-                startActivity(intent);
-            }
-        });
 
     }
 
--- app/src/main/java/com/tony/roadie/Question.java
+++ app/src/main/java/com/tony/roadie/Question.java
@@ -1,64 +1,21 @@
 package com.tony.roadie;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class Question implements Parcelable {
+public class Question {
     private String question;
     private int image;
     private String item1;
     private String item2;
     private String item3;
     private String item4;
-    private int itemNumber;
 
-    public Question(String question, int image, String item1, String item2, String item3, String item4, int itemNumber) {
+    public Question(String question, int image, String item1, String item2, String item3, String item4) {
         this.question = question;
         this.image = image;
         this.item1 = item1;
         this.item2 = item2;
         this.item3 = item3;
         this.item4 = item4;
-        this.itemNumber = itemNumber;
     }
-
-    protected Question(Parcel in) {
-        question = in.readString();
-        image = in.readInt();
-        item1 = in.readString();
-        item2 = in.readString();
-        item3 = in.readString();
-        item4 = in.readString();
-        itemNumber = in.readInt();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(question);
-        dest.writeInt(image);
-        dest.writeString(item1);
-        dest.writeString(item2);
-        dest.writeString(item3);
-        dest.writeString(item4);
-        dest.writeInt(itemNumber);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Question> CREATOR = new Creator<Question>() {
-        @Override
-        public Question createFromParcel(Parcel in) {
-            return new Question(in);
-        }
-
-        @Override
-        public Question[] newArray(int size) {
-            return new Question[size];
-        }
-    };
 
     public String getQuestion() {
         return question;
@@ -82,9 +39,5 @@ public class Question implements Parcelable {
 
     public String getItem4() {
         return item4;
-    }
-
-    public int getItemNumber() {
-        return itemNumber;
     }
 }
