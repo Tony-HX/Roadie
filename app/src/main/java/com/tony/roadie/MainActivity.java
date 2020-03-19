0256f645f8fcbe2849909b447f0f0bae42bc20d3
Tony <tony.hok-xualet@edu.itescia.fr>
2020-03-19 17:26:11 +0100

    Story 3: answers shuffle + new Question class

--- app/src/main/java/com/tony/roadie/MainActivity.java
+++ app/src/main/java/com/tony/roadie/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup radioGroup;
@@ -20,7 +25,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //adds RadioButtons to the RadioGroup on Create then shuffles the order randomly
         radioGroup = findViewById(R.id.answersRadioGroup);
+        List<RadioButton> answers = new ArrayList<>();
+        RadioButton answerRadioButton1 = new RadioButton(this);
+        RadioButton answerRadioButton2 = new RadioButton(this);
+        RadioButton answerRadioButton3 = new RadioButton(this);
+        RadioButton answerRadioButton4 = new RadioButton(this);
+        answers.add(answerRadioButton1);
+        answers.add(answerRadioButton2);
+        answers.add(answerRadioButton3);
+        answers.add(answerRadioButton4);
+        answerRadioButton1.setText("A");
+        answerRadioButton2.setText("B");
+        answerRadioButton3.setText("C");
+        answerRadioButton4.setText("D");
+        Collections.shuffle(answers);
+        for(RadioButton answerRadioButton:
+                answers){
+            radioGroup.addView(answerRadioButton, new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        }
+
         Button submitButton = findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 int radioId = radioGroup.getCheckedRadioButtonId();
                 radioButton = findViewById(radioId);
 
+                //correct answer function
                 if(radioButton.getText().equals("A")){
                     Toast.makeText(MainActivity.this, "A: Bonne réponse!", Toast.LENGTH_SHORT).show();
                 }else{
