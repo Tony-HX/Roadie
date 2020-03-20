bacceefa1c4ff65c6a6c44da38aa406fb9362795
Tony <tony.hok-xualet@edu.itescia.fr>
2020-03-20 01:02:14 +0100

    Story 8: home page that links to mainActivity

---
+++ app/src/main/java/com/tony/roadie/homeActivity.java
@@ -0,0 +1,29 @@
+package com.tony.roadie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final Button launchButton = findViewById(R.id.launchButton);
+        launchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homeActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        final Button aboutButton = findViewById(R.id.aboutButton);
+    }
+}
